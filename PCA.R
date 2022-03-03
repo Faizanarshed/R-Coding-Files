cc7cdee52afce4dbba8d92cbc483a7869e98f0de
Faizanarshed <96972195+Faizanarshed@users.noreply.github.com>
2022-03-03 13:10:57 +0500

    update

--- PCA.R
+++ PCA.R
@@ -5,31 +5,26 @@ data("iris")
 # PCA
 #x <- prcomp(iris[-5],center = TRUE,scale. = TRUE)
 x <- prcomp(iris[,-5],center = TRUE,scale. = TRUE)
-x
 summary(x)
-
 # ggplot2   GROUPING
 iris <- cbind(iris,x$x)
-
 ggplot(iris,aes(PC1,PC2,col = iris$Species,fill  = iris$Species))+
   stat_ellipse(geom = "polygon",col = "black",alpha = 0.5 )+
   geom_point(shape = 21,col = "black")+ggsave("PCA_groupping.jpeg")
-
 #PCA PLOT
 install.packages("factoextra")
 install.packages("FactoMineR")
 library(factoextra)
 library(FactoMineR)
-
 #PCA Table
 
-#iris.pca <- PCA(iris[,-5],graph = TRUE,scale.unit= TRUE)
+#iris.PCA <- PCA(iris[,-5],graph = TRUE,scale.unit= TRUE)
 
 iris.pca <- PCA(iris[,-5],graph = TRUE,scale.unit= TRUE)
 
 iris.pca <- PCA(iris[,-5],graph = TRUE,scale.unit= FALSE)
 
-# screeplot
+# scree_plot
 fviz_eig(iris.pca,addlabels = TRUE, ylim = c(0,70))
 
 # PCA Plot
